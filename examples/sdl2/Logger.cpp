43e21034fad474a50c31822c214ae3dc2c31a715
leiradel <andre@leiradella.com>
2021-10-06 21:47:53 +0100

    Pipe logs through SDL, and SDL logs through stderr

--- examples/sdl2/Logger.cpp
+++ examples/sdl2/Logger.cpp
@@ -1,25 +1,54 @@
 #include "Logger.h"
 
-#include <SDL2/SDL.h>
-
 Logger::Logger() {}
 
 bool Logger::init() {
+    _priority = SDL_LOG_PRIORITY_INFO;
+
+    SDL_LogSetOutputFunction(sdlLog, this);
     return true;
 }
 
 void Logger::destroy() {}
 
-void Logger::vprintf(retro_log_level level, char const* format, va_list args) {
-    SDL_LogPriority priority = SDL_LOG_PRIORITY_DEBUG;
+void Logger::setLevel(retro_log_level level) {
+    _priority = levelToPriority(level);
+}
 
-    switch (level) {
-        case RETRO_LOG_DUMMY: // fallthrough
-        case RETRO_LOG_DEBUG: priority = SDL_LOG_PRIORITY_DEBUG; break;
-        case RETRO_LOG_INFO: priority = SDL_LOG_PRIORITY_INFO; break;
-        case RETRO_LOG_WARN: priority = SDL_LOG_PRIORITY_WARN; break;
-        case RETRO_LOG_ERROR: priority = SDL_LOG_PRIORITY_ERROR; break;
+void Logger::vprintf(retro_log_level level, char const* format, va_list args) {
+    SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION, levelToPriority(level), format, args);
+}
+
+void Logger::sdlLog(void* userdata, int category, SDL_LogPriority priority, char const* message) {
+    auto self = static_cast<Logger*>(userdata);
+
+    if (priority < self->_priority) {
+        return;
     }
 
-    SDL_LogMessageV(SDL_LOG_CATEGORY_APPLICATION, priority, format, args);
+    char const* level = nullptr;
+
+    switch (priority) {
+        case SDL_LOG_PRIORITY_VERBOSE: level = "VERB "; break;
+        case SDL_LOG_PRIORITY_DEBUG: level = "DEBUG"; break;
+        case SDL_LOG_PRIORITY_INFO: level = "INFO "; break;
+        case SDL_LOG_PRIORITY_WARN: level = "WARN "; break;
+        case SDL_LOG_PRIORITY_ERROR: level = "ERROR"; break;
+        case SDL_LOG_PRIORITY_CRITICAL: level = "CRIT "; break;
+
+        default: level = "?????"; break;
+    }
+
+    fprintf(stderr, "[%s] %s", level, message);
+}
+
+SDL_LogPriority Logger::levelToPriority(retro_log_level level) {
+    switch (level) {
+        case RETRO_LOG_DEBUG: return SDL_LOG_PRIORITY_DEBUG;
+        case RETRO_LOG_INFO: return SDL_LOG_PRIORITY_INFO;
+        case RETRO_LOG_WARN: return SDL_LOG_PRIORITY_WARN;
+        case RETRO_LOG_ERROR: return SDL_LOG_PRIORITY_ERROR;
+
+        default: return SDL_LOG_PRIORITY_VERBOSE;
+    }
 }
--- examples/sdl2/Logger.h
+++ examples/sdl2/Logger.h
@@ -1,5 +1,7 @@
 #include <lrcpp/Components.h>
 
+#include <SDL2/SDL.h>
+
 class Logger : public lrcpp::Logger {
 public:
     Logger();
@@ -7,5 +9,13 @@ public:
     bool init();
     void destroy();
 
+    void setLevel(retro_log_level level);
+
     virtual void vprintf(retro_log_level level, char const* format, va_list args) override;
+
+protected:
+    static void sdlLog(void* userdata, int category, SDL_LogPriority priority, char const* message);
+    static SDL_LogPriority levelToPriority(retro_log_level level);
+
+    SDL_LogPriority _priority;
 };
