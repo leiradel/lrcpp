5b7228f965d36cc5393275f6c7c479e9ee8e9a2c
leiradel <andre@leiradella.com>
2021-10-06 21:49:37 +0100

    Wrote Usage(); fixed cmdline parsing; added verbose options; initialize the components

--- examples/sdl2/Player.cpp
+++ examples/sdl2/Player.cpp
@@ -7,41 +7,23 @@
 #include <stdlib.h>
 #include <string>
 
-static Logger s_logger;
-static Config s_config;
-static Video s_video;
-
-#ifdef WIN32
-    #define _MAX_PATH PATH_MAX
-
-    extern "C" {
-        #include "realpath.c"
-    }
-#endif
-
 static void Usage(FILE* out) {
-    fprintf(out, "Usage:");
-}
-
-static bool RealPath(char const* path, std::string* realPath) {
-    char temp[PATH_MAX];
-
-    if (realpath(path, temp) == NULL) {
-        fprintf(stderr, "Error: couldn't get the absolute path for \"%s\": %s", path.c_str(), strerror(errno));
-        return false;
-    }
-
-    *realPath = temp;
-    return true;
+    fprintf(out, "Usage: sdl2lrcpp [options...] <content path>\n\n");
+    fprintf(out, "-L, --libretro   The path to the libretro core\n");
+    fprintf(out, "-c, --config     The path to the configuration file (optional)\n");
+    fprintf(out, "-v, --verbose    Increase the verboseness one level (up to two levels)\n");
+    fprintf(out, "-h, --help       Shows this help screen and exit\n");
+    fprintf(out, "<content path>   The path to the content file to use with the core\n");
 }
 
 int main(int argc, char const* argv[]) {
     char const* corePathArg = nullptr;
     char const* configPathArg = nullptr;
     char const* contentPathArg = nullptr;
+    retro_log_level level = RETRO_LOG_WARN;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-L") == 0 || strcmp(argv[i], "--libretro")) {
+        if (strcmp(argv[i], "-L") == 0 || strcmp(argv[i], "--libretro") == 0) {
             if (++i == argc) {
                 fprintf(stderr, "Error: missing core path after --libretro\n");
                 return EXIT_FAILURE;
@@ -54,7 +36,7 @@ int main(int argc, char const* argv[]) {
 
             corePathArg = argv[i];
         }
-        else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--config")) {
+        else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--config") == 0) {
             if (++i == argc) {
                 fprintf(stderr, "Error: missing configuration path after --config\n");
                 return EXIT_FAILURE;
@@ -66,6 +48,18 @@ int main(int argc, char const* argv[]) {
             }
 
             configPathArg = argv[i];
+        }
+        else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+            if (level == RETRO_LOG_WARN) {
+                level = RETRO_LOG_INFO;
+            }
+            else if (level == RETRO_LOG_INFO) {
+                level = RETRO_LOG_DEBUG;
+            }
+            else {
+                fprintf(stderr, "Error: cannot be more verbose\n");
+                return EXIT_FAILURE;
+            }
         }
         else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
             Usage(stdout);
@@ -86,12 +80,35 @@ int main(int argc, char const* argv[]) {
         return EXIT_FAILURE;
     }
 
-    std::string corePath;
-    std::string contentPath;
+    Logger logger;
 
-    if (!RealPath(corePathArg, &corePath) || !RealPath(contentPathArg, &contentPath)) {
+    if (!logger.init()) {
+        fprintf(stderr, "Error: could not initialize the logger");
         return EXIT_FAILURE;
     }
+
+    logger.setLevel(level);
+
+    Config config;
+
+    if (!config.init(configPathArg, contentPathArg, corePathArg, &logger)) {
+        fprintf(stderr, "Error: could not initialize the configuration component");
+        logger.destroy();
+        return EXIT_FAILURE;
+    }
+
+    Video video;
+
+    if (!video.init(&logger)) {
+        fprintf(stderr, "Error: could not initialize the video component");
+        config.destroy();
+        logger.destroy();
+        return EXIT_FAILURE;
+    }
+
+    video.destroy();
+    config.destroy();
+    logger.destroy();
 
     return EXIT_SUCCESS;
 }
