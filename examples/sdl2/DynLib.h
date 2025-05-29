40014dd7a40ee6d7cdc29436518eb82c67b2c17f
frysee <frysee@googlemail.com>
2025-05-29 23:20:23 +0200

    chore: moved dynlib handling to example code, removed extra abstracttion

--- examples/sdl2/DynLib.h
+++ examples/sdl2/DynLib.h
@@ -1,7 +1,16 @@
 #pragma once
 
-#include "lrcpp/dynlib.h"
 #include <string>
+
+#ifdef _WIN32
+#   include <stdio.h>
+#   define WIN32_LEAN_AND_MEAN
+#   include <Windows.h>
+    typedef HMODULE dynlib_t;
+#else
+#   include <dlfcn.h>
+    typedef void* dynlib_t;
+#endif
 
 // C++ wrapper around lrcpp/dynlib.h
 class DynLib {
@@ -11,27 +20,59 @@ public:
 
     bool load(const std::string& path) {
         unload(); // Unload previous if any
-        handle = dynlib_open(path.c_str());
+#ifdef _WIN32
+        handle = LoadLibrary(path.c_str());
+#else
+        handle = dlopen(path.c_str(), RTLD_LAZY);
+#endif
         return handle != nullptr;
     }
 
     void unload() {
         if (handle) {
-            dynlib_close(handle);
+#ifdef _WIN32
+            FreeLibrary(lib);
+#else
+            dlclose(handle);
+#endif
             handle = nullptr;
         }
     }
 
     void* getSymbol(const char* name) const {
         if (!handle) return nullptr;
-        return dynlib_symbol(handle, name);
+#ifdef _WIN32
+        return static_cast<void *>(GetProcAddress(handle, name));
+#else
+        return dlsym(handle, name);
+#endif
     }
 
     const char* error() const {
 #ifdef _WIN32
-        return dynlib_error(); // stub; Windows version may need implementation
+        static char msg[ 512 ];
+        
+        DWORD err = GetLastError();
+
+        DWORD res = FormatMessage(
+            FORMAT_MESSAGE_FROM_SYSTEM,
+            NULL,
+            err,
+            MAKELANGID( LANG_ENGLISH, SUBLANG_DEFAULT ),
+            msg,
+            sizeof( msg ) - 1,
+            NULL
+        );
+
+        if ( res == 0 )
+        {
+            snprintf( msg, sizeof( msg ) - 1, "Error %lu", err );
+            msg[ sizeof( msg ) - 1 ] = 0;
+        }
+
+        return msg;
 #else
-        return dynlib_error();
+        return dlerror();
 #endif
     }
 
